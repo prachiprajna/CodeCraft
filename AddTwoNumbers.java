e03fe8089f65c945b6ed6a8e49a4f3aee6a3c9b5
prachiprajna <240301120417@centurionuniv.edu.in>
2025-07-10 20:21:38 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,8 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
